32e099467086038ba9e1ba86b86cd2888bec5117
Emmanuel VAISSE <evaisse@gmail.com>
2015-06-16 16:53:58 +0200

    [json] fix invalid method json_last_error_msg

--- Http/Response.php
+++ Http/Response.php
@@ -80,10 +80,35 @@ class Response extends \Symfony\Component\HttpFoundation\Response
         if ($this->headers->get('content-type') === "application/json") {
             $result = json_decode($this->getContent(), true);
             if (json_last_error()) {
-                $this->error = new InvalidResponseBodyException($this, 'Invalid Json response body. ' . json_last_error_msg());
+                $this->error = new InvalidResponseBodyException($this, 'Invalid Json response body. ' . $this->getJsonLastErrorMessage());
             }
             $this->setResult($result);
         }
+    }
+
+
+    /**
+     * wrap json_last_error_msg() function for php<5.5 versions
+     *
+     * @return string last json decoding/encoding error
+     */
+    protected function getJsonLastErrorMessage()
+    {
+        if (function_exists('json_last_error_msg')) {
+            return json_last_error_msg();
+        }
+
+        static $errors = array(
+            JSON_ERROR_NONE             => null,
+            JSON_ERROR_DEPTH            => 'Maximum stack depth exceeded',
+            JSON_ERROR_STATE_MISMATCH   => 'Underflow or the modes mismatch',
+            JSON_ERROR_CTRL_CHAR        => 'Unexpected control character found',
+            JSON_ERROR_SYNTAX           => 'Syntax error, malformed JSON',
+            JSON_ERROR_UTF8             => 'Malformed UTF-8 characters, possibly incorrectly encoded'
+        );
+
+        $error = json_last_error();
+        return array_key_exists($error, $errors) ? $errors[$error] : "Unknown error ({$error})";
     }
 
     /** 
